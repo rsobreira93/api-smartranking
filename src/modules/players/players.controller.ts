46d339f2bfdd5ea7f191bd964f056c341b681113
rsobreira93 <sobreiraromuloes@gmail.com>
2023-01-22 08:00:06 -0300

    refactor: adjust and return and add constructor

--- src/modules/players/players.controller.ts
+++ src/modules/players/players.controller.ts
@@ -1,16 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
 import { CreatePlayerDTO } from './dtos/create-player-dto';
+import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
+
   @Post()
   async createPlayer(@Body() createPlayerDTO: CreatePlayerDTO) {
     const { email, name, phone } = createPlayerDTO;
-    return JSON.stringify({
-      name: `${name}`,
-      phone: `${phone}`,
-      email: `${email}`,
+
+    const playerService = await this.playersService.execute({
+      email,
+      name,
+      phone,
     });
+
+    return playerService;
   }
 }
